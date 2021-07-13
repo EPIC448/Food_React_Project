ac18363f5f0cba61aa9988666674bec46ae5be9f
EPIC448 <samrey2018@gmail.com>
2021-07-12 22:41:35 -0400

    Added state to the parents and child components for this project

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import { View, TextInput, StyleSheet } from 'react-native';
 import { Feather } from '@expo/vector-icons';
-const SearchBar = () => {
+
+const SearchBar = ({ term, onTermChange }) => {
   return (
     <View style={styles.backgroundStyle}>
       <Feather name="search" style={ styles.iconStyle} />
-      <TextInput style={styles.inputStyle} placeholder="Search" />
+      <TextInput
+        autoCapitalize="none"
+        autoCorrect={false}
+        style={styles.inputStyle}
+        placeholder="Search"
+        value={term}
+        onChangeText={newTerm => onTermChange(newTerm)}
+        onEndEditing={() => console.log(' submitted ')}
+      />
     </View>
   );
 };
