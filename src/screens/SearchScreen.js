5641d60c919416654123e5e0cf639563c839df33
EPIC448 <samrey2018@gmail.com>
2021-07-21 16:46:58 -0400

    Group Results Spending

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,34 +1,36 @@
-import React, { useState }from 'react';
+import React, { useState,useEffect }from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
 import yelp from '../api/yelp';
+import useResults from "../hooks/useResults";
+import ResultsList from '../components/ResultsList';
+
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
-  const [results, setResults] = useState([])
+  const [searchApi, results, errorMessage] = useResults();
 
-  const searchApi = async () => {
-    const response = await yelp.get('/search', {
-      params: {
-        limit: 50,
-        term,
-        location: 'san jose'
-      }
-    });
-    setResults(response.data.businesses)
+
+  const filterResultByPrice = (price) => {
+
+    //price === '$' || '$$' || '$$$'
+    return results.filter(result => {
+      return result.price === price;
+    })
   };
-
+  
   return (
     <View>
       <SearchBar
         term={term}
         onTermChange={setTerm} 
-        onTermSubmit={searchApi()}
+        onTermSubmit={() => searchApi(term)}
       />
-      <Text>Search Screen</Text>
-      <Text>
-      we found this {results.length} Number for sdsad
-      </Text>
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
+      <Text>we found this {results.length} Number for sdsad</Text>
+      <ResultsList results={filterResultByPrice('$')}title = "Cost Effective" />
+      <ResultsList results={filterResultByPrice('$$')}title = "Bit Pricer"/>
+      <ResultsList results={filterResultByPrice('$$$')}title = "Big Spender"/>
     </View>
   );
 };
