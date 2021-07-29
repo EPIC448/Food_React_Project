151b0e4351ff013b3e12ed3e0eb6c09409aac1a9
EPIC448 <samrey2018@gmail.com>
2021-07-28 22:00:00 -0400

    ability to show a single item when it is clicked on

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -6,7 +6,7 @@ import useResults from "../hooks/useResults";
 import ResultsList from '../components/ResultsList';
 
 
-const SearchScreen = () => {
+const SearchScreen = ({ navigation}) => {
   const [term, setTerm] = useState('');
   const [searchApi, results, errorMessage] = useResults();
 
@@ -29,9 +29,9 @@ const SearchScreen = () => {
       {errorMessage ? <Text>{errorMessage}</Text> : null}
       <ScrollView>
 
-        <ResultsList results={filterResultByPrice('$')}title = "Cost Effective" />
-        <ResultsList results={filterResultByPrice('$$')}title = "Bit Pricer"/>
-        <ResultsList results={filterResultByPrice('$$$')} title="Big Spender" />
+        <ResultsList results={filterResultByPrice('$')}title = "Cost Effective" navigation={navigation} />
+        <ResultsList results={filterResultByPrice('$$')}title = "Bit Pricer" navigation={navigation}/>
+        <ResultsList results={filterResultByPrice('$$$')} title="Big Spender" navigation={navigation} />
         
       </ScrollView>
     </>
